f29b5e401d633c2544b4811471e36ad79007821d
Brooks Patton <brooks@brookzerker.com>
2016-11-04 14:38:28 -0600

    the square falls with gravity

--- main.lua
+++ main.lua
@@ -1,7 +1,17 @@
+function love.load()
+  birdY = 200
+  birdYSpeed = 0
+end
+
+function love.update(dt)
+  birdYSpeed = birdYSpeed + (516 * dt)
+  birdY = birdY + (birdYSpeed * dt)
+end
+
 function love.draw()
   love.graphics.setColor(35, 92, 118)
   love.graphics.rectangle('fill', 0, 0, 300, 388)
 
   love.graphics.setColor(224, 214, 68)
-  love.graphics.rectangle('fill', 62, 200, 30, 25)
+  love.graphics.rectangle('fill', 62, birdY, 30, 25)
 end
